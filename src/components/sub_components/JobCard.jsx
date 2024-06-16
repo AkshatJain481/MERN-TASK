e7fbecc46c34d7d9e42afc088d70de8e5ce58b5a
Akshat <akshatjain481@gmail.com>
2024-06-17 00:22:04 +0530

    fixed some bugs

--- src/components/sub_components/JobCard.jsx
+++ src/components/sub_components/JobCard.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react';
 
 function JobCard({ type, title, company, location, authorImg, author}) {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className="bg-white rounded shadow  border border-[#E0E0E0] rounded-md inline-block mt-4">
       
@@ -13,6 +33,22 @@ function JobCard({ type, title, company, location, authorImg, author}) {
                 <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg" >
                 <path d="M18.6667 14C18.6667 15.2833 19.7167 16.3333 21 16.3333C22.2834 16.3333 23.3334 15.2833 23.3334 14C23.3334 12.7166 22.2834 11.6666 21 11.6666C19.7167 11.6666 18.6667 12.7166 18.6667 14ZM16.3334 14C16.3334 12.7166 15.2834 11.6666 14 11.6666C12.7167 11.6666 11.6667 12.7166 11.6667 14C11.6667 15.2833 12.7167 16.3333 14 16.3333C15.2834 16.3333 16.3334 15.2833 16.3334 14ZM9.33337 14C9.33337 12.7166 8.28337 11.6666 7.00004 11.6666C5.71671 11.6666 4.66671 12.7166 4.66671 14C4.66671 15.2833 5.71671 16.3333 7.00004 16.3333C8.28337 16.3333 9.33337 15.2833 9.33337 14Z" fill="black"/>
                 </svg>
+                {isOpen && (
+                <div
+                  ref={dropdownRef}
+                  className="z-20 absolute mt-[170px] bg-white shadow-xl rounded-lg py-2 w-[200px] mr-[170px]"
+                >
+                  <a href="#" className="block px-4 py-2 hover:bg-gray-100">
+                  Report
+                  </a>
+                  <a href="#" className="block px-4 py-2 hover:bg-gray-100">
+                  Edit
+                  </a>
+                  <a href="#" className="block px-4 py-2 hover:bg-gray-100">
+                  Save
+                  </a>
+                </div>
+              )}
               </button>
             </div>
             <div className='flex items-center space-x-12'>
