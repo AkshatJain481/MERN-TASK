a91d08c958aaa959266d4f10c98480ba844bb439
Akshat <akshatjain481@gmail.com>
2024-06-17 00:25:22 +0530

    fixed more bugs

--- src/components/sub_components/JobCard.jsx
+++ src/components/sub_components/JobCard.jsx
@@ -29,7 +29,7 @@ function JobCard({ type, title, company, location, authorImg, author}) {
             <div className=' font-semibold '>{type}</div>
             <div className='flex justify-between'>
               <h2 className="text-xl font-bold py-2 max-w-[570px]">{title}</h2>
-              <button className='w-10 h-8 hover:bg-gray-400 flex items-center justify-center rounded-[4px] ml-[20px]'>
+              <button className='w-10 h-8 hover:bg-gray-400 flex items-center justify-center rounded-[4px] ml-[20px]' onClick={toggleDropdown}>
                 <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg" >
                 <path d="M18.6667 14C18.6667 15.2833 19.7167 16.3333 21 16.3333C22.2834 16.3333 23.3334 15.2833 23.3334 14C23.3334 12.7166 22.2834 11.6666 21 11.6666C19.7167 11.6666 18.6667 12.7166 18.6667 14ZM16.3334 14C16.3334 12.7166 15.2834 11.6666 14 11.6666C12.7167 11.6666 11.6667 12.7166 11.6667 14C11.6667 15.2833 12.7167 16.3333 14 16.3333C15.2834 16.3333 16.3334 15.2833 16.3334 14ZM9.33337 14C9.33337 12.7166 8.28337 11.6666 7.00004 11.6666C5.71671 11.6666 4.66671 12.7166 4.66671 14C4.66671 15.2833 5.71671 16.3333 7.00004 16.3333C8.28337 16.3333 9.33337 15.2833 9.33337 14Z" fill="black"/>
                 </svg>
